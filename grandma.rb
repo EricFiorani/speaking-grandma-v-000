f630e3a0c3bdabcf8dada604284ffc2be27469ac
Eric Fiorani <efiorani305@gmail.com>
2018-04-05 16:33:36 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,8 +13,8 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
   if phrase != phrase.uppercase
-    return "HUH?! SPEAK UP, SONNY!"
+    puts "HUH?! SPEAK UP, SONNY!"
   else
-    return "NO, NOT SINCE 1938!"
+    puts "NO, NOT SINCE 1938!"
   end
 end
