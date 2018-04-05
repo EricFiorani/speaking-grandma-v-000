ec0e8b183fc8e91ae09f5d6310b9ff7cc14171c9
Eric Fiorani <efiorani305@gmail.com>
2018-04-05 16:33:21 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,6 +15,6 @@ def speak_to_grandma(phrase)
   if phrase != phrase.uppercase
     return "HUH?! SPEAK UP, SONNY!"
   else
-    return "return "NO, NOT SINCE 1938!"
+    return NO, NOT SINCE 1938!"
   end
 end
