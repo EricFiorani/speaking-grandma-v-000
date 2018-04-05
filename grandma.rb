a2f0cc08071054a13115b823a729a8c9865f0519
Eric Fiorani <efiorani305@gmail.com>
2018-04-05 16:32:50 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,6 @@ def speak_to_grandma(phrase)
     phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else
-    phrase == phrase.uppercase
     return "NO, NOT SINCE 1938!"
   end
 end
